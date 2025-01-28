961c0100d8c36295a1ad8dd64b3c18a4aedfe8f8
alexblack8-cs2 <ablack27@riverdale.edu>
2025-01-28 11:38:26 -0500

    Finished code, not finished project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Once upon a time, a dozen " + args[0] + " were " + args[1] + " in " + args[3] + ".");
+        System.out.println("The " + args[2] + " things strolled the land, until getting hungry for" + args[4] + ".");
+       System.out.println("At the "+args[4]+" place, they saw "+args[6]+args[7]+".");
     }
 }
